3b70ab502cb6e75010516ad57c8db638d07b1d20
gabrielgio <gabrielgio@localhost.localdomain>
2017-05-06 23:09:07 -0300

    Initial commit

---
+++ doc/intro.md
@@ -0,0 +1,3 @@
+# Introduction to track-worker
+
+TODO: write [great documentation](http://jacobian.org/writing/what-to-write/)
---
+++ src/track_worker/core.clj
@@ -0,0 +1,7 @@
+(ns track-worker.core
+  (:gen-class))
+
+(defn -main
+  "I don't do a whole lot ... yet."
+  [& args]
+  (println "Hello, World!"))

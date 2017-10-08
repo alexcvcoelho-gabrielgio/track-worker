8ca36e1a5f34dd6d966ba6ff0650b44137cc3d2f
gabriel giovanini <gabrielgio@localhost.localdomain>
2017-10-08 15:47:27 -0300

    Adds feedback, not the best way to do it, it is all I have time do now

--- src/track_worker/core.clj
+++ src/track_worker/core.clj
@@ -7,19 +7,21 @@
             [clojure.data.json :as json]))
 
 (defn pull [item]
-    (case (:command item)
-      "track" (rt/save-track item)
-      nil))
+  (println (:command item) " " (:session-id item))
+  (case (:command item)
+    "track" (rt/save-track item)
+    nil))
 
 (defn loop-through [c]
-  (loop []
+  (loop [count 0]
     (try
+      (println "LOOP " count)
       (kf/pop-track-async c)
       (doseq [i (<!! c)]
         (pull i))
       (catch Exception e
         (-> e print)))
-    (recur)))
+    (recur (inc count))))
 
 (defn -main [& args]
   (mount.core/start)
